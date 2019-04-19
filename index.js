969cfe74ecd1d2f4cc7b046744d092096e1b20a0
mdons <michaeljdonnell@gmail.com>
2019-04-18 20:54:50 -0500

    end of class

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const TicketRoutes = require('./express/routes/tickets')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const TicketRoutes = require("./express/routes/tickets");
 
-mongoose.connect(/* ADD CONNECTION STRING */)
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0-givgy.mongodb.net/test?retryWrites=true"
+);
 
-const port = process.env.PORT || 4001
-const app = express()
+const port = process.env.PORT || 4001;
+const app = express();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'))
+  app.use(express.static("client/build"));
 }
 
-app.use(bodyParser.json())
-app.use('/api/tickets', TicketRoutes)
+app.use(bodyParser.json());
+app.use("/api/tickets", TicketRoutes);
 
-app.get('/', (req, res) => res.send('Default route!'))
+app.get("/", (req, res) => res.send("Default route!"));
 
 app.listen(port, () => {
-  console.log(`Express app running on localhost:${port}`)
-})
+  console.log(`Express app running on localhost:${port}`);
+});
